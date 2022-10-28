f506a57df34c26ea33c35232d008238cdf5a2a89
naamarin <naamarin@g.jct.ac.il>
2022-10-28 14:15:19 +0300

    Initial code + welcome function

---
+++ Stage0/Program9204.cs
@@ -0,0 +1,17 @@
+﻿partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome9204();
+        Welcome9287();
+        Console.ReadKey();
+    }
+    private static void Welcome9204()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+    static partial void Welcome9287();
+    
+}
