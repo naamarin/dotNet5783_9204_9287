bc8d78dfa6ea1ae151fee4530295a275e8c1a414
shoham212 <shoamhadar@gmail.com>
2022-10-30 18:47:31 +0200

    changes

---
+++ Stage0/Program9287.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Wellcome9287()
+        { Console.WriteLine("I'm also here!");} 
+    }
+}
