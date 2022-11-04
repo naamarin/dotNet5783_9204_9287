3c33f7a32c07dd5ab9b4071a85504d7ded487ad0
shoham212 <shoamhadar@gmail.com>
2022-11-04 12:18:17 +0200

    starting of the project- stage one

---
+++ DalFacade/DO/Order.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace DO;
+
+/// <summary>
+/// This struct describes a single customer order 
+/// </summary>
+public struct Order
+{
+    /// <summary>
+    /// Unique ID number of an order
+    /// </summary>
+    public int ID { get; set; }
+    /// <summary>
+    /// The customer's name
+    /// </summary>
+    public string CustomerName { get; set; }
+    /// <summary>
+    /// Customer's cell phone number
+    /// </summary>
+    public int CustomerNumber { get; set; }
+    /// <summary>
+    /// Shipping address
+    /// </summary>
+    public string CustomerAdress { get; set; }
+    /// <summary>
+    /// Order creation date
+    /// </summary>
+    public DateTime OrderDate { get; set; }
+    /// <summary>
+    /// delivery date
+    /// </summary>
+    public DateTime ShipDate { get; set; }
+    /// <summary>
+    /// Date of delivery
+    /// </summary>
+    public DateTime DeliveryDate { get; set; }
+
+    /// <summary>
+    /// A function that prints all the order details
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => $@"
+    ID: {ID}
+    Customer Name: {CustomerName}
+    Customer Number: {CustomerNumber}
+    customer Address: {CustomerAdress}
+    Order Date: {OrderDate}
+    Ship Date: {ShipDate}
+    Delivery Date: {DeliveryDate}
+    ";
+}
+
---
+++ DalFacade/DO/Product.cs
@@ -0,0 +1,40 @@
+﻿
+namespace DO;
+/// <summary>
+/// This stuct describes a single product in the store
+/// </summary>
+public struct Product
+{
+    /// <summary>
+    /// Unique ID number of a product
+    /// </summary>
+    public int ID { get; set; }
+    /// <summary>
+    /// Product Name
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// The product category
+    /// </summary>
+    public double Price { get; set; }
+    /// <summary>
+    /// The price of the product
+    /// </summary>
+    public string Category { get; set; }
+    /// <summary>
+    /// Product quantity in stock
+    /// </summary>
+    public int InStock { get; set; }
+
+    /// <summary>
+    /// A function that prints all the product details
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => $@"
+    Product ID={ID}: {Name}, 
+    category - {Category}
+    Price: {Price}
+    Amount in stock: {InStock}
+";
+
+}
