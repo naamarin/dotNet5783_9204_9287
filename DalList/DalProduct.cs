12c34a47451a10933e597f3f5eba6a164781a6e5
shoham212 <shoamhadar@gmail.com>
2022-12-04 18:02:10 +0200

    רעארע

--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -5,13 +5,22 @@ namespace Dal;
 
 public class DalProduct
 {
-
+    /// <summary>
+    ///Function to create a new product
+    /// </summary>
+    /// <param name="product"></param>
+    /// <returns></returns>
     public int Add(Product product)
     {
         DataSource.ProductList.Add(product);
         return product.ID;
     }
-
+    /// <summary>
+    /// A function to return an existing product
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
     public Product GetById(int id)
     {
         if (!DataSource.ProductList.Exists(x => x?.ID == id))
@@ -20,7 +29,11 @@ public class DalProduct
         }
         return (Product)DataSource.ProductList.Find(x => x?.ID == id);
     }
-
+    /// <summary>
+    /// Product update function
+    /// </summary>
+    /// <param name="product"></param>
+    /// <exception cref="Exception"></exception>
     public void Update(Product product)
     {
         if (!DataSource.ProductList.Exists(x => x?.ID == product.ID))
@@ -30,7 +43,11 @@ public class DalProduct
         DataSource.ProductList.Remove(DataSource.ProductList.Find(x => x?.ID == product.ID));
         DataSource.ProductList.Add(product);
     }
-
+    /// <summary>
+    /// A function to delete an existing product
+    /// </summary>
+    /// <param name="id"></param>
+    /// <exception cref="Exception"></exception>
     public void Delete(int id)
     {
         if (!DataSource.ProductList.Exists(x => x?.ID == id))
@@ -39,7 +56,10 @@ public class DalProduct
         }
         DataSource.ProductList.Remove(DataSource.ProductList.Find(x => x?.ID == id));
     }
-
+    /// <summary>
+    /// Function to return all products
+    /// </summary>
+    /// <returns></returns>
     public IEnumerable<Product?> GetAll()
     {
         List<Product?> newList = new List<Product?>();
