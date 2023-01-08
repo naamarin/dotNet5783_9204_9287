c83b193ae6fa6d77535f424956f2f098829409b8
shoham212 <shoamhadar@gmail.com>
2023-01-08 23:07:42 +0200

    images

---
+++ PL/Converters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace PL
+{
+    class ConvertImagePathToBitmap : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string imageRelativeName=(string)value;
+                string currentDir = Environment.CurrentDirectory[..^4];
+                string imageFullName = currentDir + imageRelativeName;
+                BitmapImage bitmapImage = new BitmapImage(new Uri(imageFullName));
+                return bitmapImage;
+            }
+            catch (Exception ex)
+            {
+                string imageRelativeName = @"";
+                string currentDir = Environment.CurrentDirectory[..^4];
+                string imageFullName = currentDir + imageRelativeName;
+                BitmapImage bitmapImage = new BitmapImage(new Uri(imageFullName));
+                return bitmapImage;
+
+            }
+        }
+        public object ConvertBack(object value,Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
